c20f051fc19110bf5acf01b74c86388def11a77f
Gabriel Yara Fracaro <gabrielyarafracaro@icloud.com>
2025-11-18 22:11:16 -0300

    Delete routes.js

--- routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const tasksController = require('../controllers/tasksController');
-
-router.get('/', tasksController.getAllTasks);
-router.post('/', tasksController.createTask);
-
-module.exports = router;
